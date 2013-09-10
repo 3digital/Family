bb7e6474699e5913b69622ff0fa57920d9b82565
Adam Schwem <schwem@learnovatelabs.com>
2013-09-10 10:24:55 -0500

    Updated spellings and toString() implemenation

--- Family/src/main/java/com/delta/family/FamilyMember.java
+++ Family/src/main/java/com/delta/family/FamilyMember.java
@@ -5,9 +5,10 @@ package com.delta.family;
  */
 public class FamilyMember {
     private String myFirstName;
-    private static String ourLastName = "Android";
+    private static String ourLastName = "Reynolds";
     private Integer myAge;
-    private static String ourAddress = "Austin, TX";
+    private static String ourAddress = "912 Congress Ave, Austin TX";
+    private String myAddress;
     private static String ourPhoneNumber = "(555)512-1345";
     private String myPhoneNumber;
 
@@ -16,11 +17,11 @@ public class FamilyMember {
     }
 
     public String getMyName() {
-        //todo:return FamilyMember's age
+        //todo:return FamilyMember's full name
         return null;
     }
 
-    public static Integer getMyAge() {
+    public Integer getMyAge() {
         //todo:return FamilyMember's age
         return null;
     }
@@ -40,9 +41,25 @@ public class FamilyMember {
         return null;
     }
 
+    public static String getMyAddress(FamilyMember familyMember)
+    {
+        //todo: return in this format: "<family-member-name> lives at <address>"
+        return null;
+    }
+
+
     @Override
     public String toString() {
-        String result = "Info goes here";
+        String result;
+        try{
+            result = getMyName() + " , Age:" + getMyAge().toString() + "\n" +
+                        "Personal: " + callMe(this) + "\n" +
+                        "Landline:" + callLandLine() + "\n" +
+                        "Address:" + getMyAddress(this);
+        }catch(Exception e){
+            result = "incomplete implementation";
+        }
+
         return result;
     }
 }
